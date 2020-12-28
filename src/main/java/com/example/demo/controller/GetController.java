403dcee58cf1c907c13cc99ec54096c1c2f1f4c3
YJLII <destprato@gmail.com>
2020-12-28 21:41:38 +0900

    Commit Website Project. Tests don't pass

---
+++ src/main/java/com/example/demo/controller/GetController.java
@@ -0,0 +1,34 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.SearchParam;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")//Localhost:8008/api
+public class GetController {
+
+
+    @RequestMapping(method = RequestMethod.GET,path="/getMethod")//Localhost:8080/api/getMethod
+    public String getRequest(){
+
+        return "Hi getMethod";
+    }
+
+    @GetMapping("/getParameter")//Localhost:8008/api/getParameter
+    public String getParameter(@RequestParam String id, @RequestParam String password){
+        System.out.println("id"+id);
+        System.out.println("Password"+password);
+
+        return id+password;
+    }
+
+    @GetMapping("/getMultiParameter")
+    public SearchParam getMultiParameter(SearchParam searchParam )
+    {
+        System.out.println(searchParam.getAccount());
+        System.out.println(searchParam.getEmail());
+        System.out.println(searchParam.getPage());
+        return searchParam;
+    }
+
+}
